32ada80a32dd2e22a59643b58e65922079316fc9
Ben-Sicat <bensicat00@gmail.com>
2023-04-06 18:23:54 +0800

    update

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Login  from './pages/index'
+
+function App() {
+  return (
+    <div className="app">
+      
+    </div>
+  );
+}
+
+export default App;
