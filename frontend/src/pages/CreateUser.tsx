2b5a9026d9ac2d3f5ff1deaa5305415dc01c9f4a
Ben-Sicat <bensicat00@gmail.com>
2023-04-08 19:58:03 +0800

    tix #8 done

--- frontend/src/pages/CreateUser.tsx
+++ frontend/src/pages/CreateUser.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Button, Container, Typography } from '@mui/material';
+import { Button, Container, Typography, Grid } from '@mui/material';
 import {Textfield} from '../components/index';
 
 interface User {
@@ -51,37 +51,49 @@ const CreateUser = () => {
         Create User
       </Typography>
       <form onSubmit={handleSubmit}>
-        <Textfield
-          label="Name"
-          name="name"
-          value={user.name}
-          onChange={handleInputChange}
-        />
-        <Textfield
-          label="Age"
-          name="age"
-          type="number"
-          value={user.age}
-          onChange={handleInputChange}
-        />
-        <Textfield
-          label="Birthday"
-          name="birthday"
-          value={user.birthday}
-          onChange={handleInputChange}
-        />
-        <Textfield
-          label="Address"
-          name="address"
-          value={user.address}
-          onChange={handleInputChange}
-        />
-        <Textfield
-          label="Contact Number"
-          name="contactNumber"
-          value={user.contactNumber}
-          onChange={handleInputChange}
-        />
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Textfield
+              label="Name"
+              name="name"
+              value={user.name}
+              onChange={handleInputChange}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <Textfield
+              label="Age"
+              name="age"
+              type="number"
+              value={user.age}
+              onChange={handleInputChange}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <Textfield
+              label="Birthday"
+              name="birthday"
+              value={user.birthday}
+              onChange={handleInputChange}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Textfield
+              label="Address"
+              name="address"
+              value={user.address}
+              onChange={handleInputChange}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Textfield
+              label="Contact Number"
+              name="contactNumber"
+              value={user.contactNumber}
+              onChange={handleInputChange}
+            />
+          </Grid>
+        </Grid>
         <Button variant="contained" color="primary" type="submit" fullWidth>
           Create
         </Button>
