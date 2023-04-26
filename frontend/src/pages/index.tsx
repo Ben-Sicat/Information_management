3bc53976fd964e655a849c9a0bf8ea708bbc84ec
Dezignful <wolf.lance4@gmail.com>
2023-04-26 20:39:35 +0800

    Added 404 Error Message

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,10 +1,19 @@
-import Login from "./Login"
-import Dashboard from "./Dashboard"
-import CreateUser from "./CreateUser"
-import Registration from "./Registration"
-import About from "./About";
-import Main from "./Main"
-import UserProfile from "./UserProfile";
+import Login from './Login';
+import Dashboard from './Dashboard';
+import CreateUser from './CreateUser';
+import Registration from './Registration';
+import About from './About';
+import Main from './Main';
+import UserProfile from './UserProfile';
+import Error404 from './Error404';
 
-
-export {Login, Dashboard , CreateUser, Registration, About, Main, UserProfile}
+export {
+	Login,
+	Dashboard,
+	CreateUser,
+	Registration,
+	About,
+	Main,
+	UserProfile,
+	Error404,
+};
