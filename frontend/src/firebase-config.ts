a649dd83db690ed38aa968f0da567e9f9cce45bb
Ben-Sicat <bensicat00@gmail.com>
2023-05-02 18:16:19 +0800

    new db

--- frontend/src/firebase-config.ts
+++ frontend/src/firebase-config.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBR-LYLfcPn4dXdOjq-hpYSNoEX9x9IA0I",
-    authDomain: "senior607-6da5c.firebaseapp.com",
-    projectId: "senior607-6da5c",
-    storageBucket: "senior607-6da5c.appspot.com",
-    messagingSenderId: "1093059817556",
-    appId: "1:1093059817556:web:893f110f754be32a4a3dc1",
-    measurementId: "G-LN7Y1EP3S4"
+    apiKey: "AIzaSyAdtYdWyw2r9CKYl2nElrOHggmQUr6kbrw",
+    authDomain: "baranggay670.firebaseapp.com",
+    projectId: "baranggay670",
+    storageBucket: "baranggay670.appspot.com",
+    messagingSenderId: "873567954454",
+    appId: "1:873567954454:web:202043039a2c470ffeb737",
+    measurementId: "G-0DMJXYSPZ5"
 };
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
