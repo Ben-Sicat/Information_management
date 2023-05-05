7f2890b749773dd96ef630746d9e5f1761ff9bf2
NeroXerith <biene.bryle.sanico@adamson.edu.ph>
2023-05-05 10:41:45 +0800

    Fixed Navbar, Footer

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,34 +1,28 @@
-
-import { Datagrid, Footer } from '../components/index';
-
-import CustomButton from '../components/Buttons';
+import React, { useState, useEffect } from 'react';
+import { Datagrid, Navbar } from '../components/index';
+import { getAuth, signOut } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
 import { styled } from '@mui/system';
-import { CssBaseline,} from '@mui/material';
-// import { lightTheme, darkTheme } from '../theme';
-const BodyContainer = styled('div')`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin: 10px;
-`;
+import { Button} from '@mui/material';
+
+
 const ButtonContainer = styled('div')`
-  position: fixed;
-  bottom: 10rem;
-  right: 10rem;
+  position: static;
+  bottom: 1rem;
+  right: 1rem;
   z-index: 999;
-  border-radius: 10rem;
-  background-color: red;
 `;
 
 
 const Dashboard: React.FC = () => {
-  // const [scrolled, setScrolled] = useState(false);
-
+  const [scrolled, setScrolled] = useState(false);
+  const auth = getAuth();
 
  
 
-
+  const handleSignOut = () => {
+    signOut(auth);
+  };
 
   const navigate = useNavigate();
   const handleAddNowClick = () => {
@@ -38,20 +32,9 @@ const Dashboard: React.FC = () => {
     
   return (
     <>
-    {/* <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}> */}
-    <CssBaseline />
-      <BodyContainer sx={{
-        			'@media screen and (max-width: 899px)': {
-                marginBottom: '100%'
-                },
-      }}>
       <Datagrid />
-      </BodyContainer>
-      <ButtonContainer>
-        <CustomButton label="+"  onClick={handleAddNowClick} sx={{ color: 'secondary'}}/>
-      </ButtonContainer>
-      <Footer/>      
-            {/* </ThemeProvider> */}
+
+      
     </>
   );
 };
