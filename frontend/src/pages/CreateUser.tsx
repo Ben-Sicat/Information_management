1e9e98f5996f82b24d8286259424013e00b5072c
NeroXerith <biene.bryle.sanico@adamson.edu.ph>
2023-05-14 17:01:34 +0800

    Removing unnecessary imports and functions

--- frontend/src/pages/CreateUser.tsx
+++ frontend/src/pages/CreateUser.tsx
@@ -11,7 +11,7 @@ import {
 	TextField,
 	Typography,
 } from '@mui/material';
-import { CollectionReference, DocumentData, collection, doc, getDoc, setDoc } from 'firebase/firestore';
+import { collection, doc, getDoc, setDoc } from 'firebase/firestore';
 import { db } from '../firebase-config';
 
 interface User {
@@ -250,7 +250,5 @@ const AddProfile: React.FC = () => {
 };
 
 export default AddProfile;
-function addDoc(userCollectionRef: CollectionReference<DocumentData>, editedUser: User) {
-	throw new Error('Function not implemented.');
-}
+
 
