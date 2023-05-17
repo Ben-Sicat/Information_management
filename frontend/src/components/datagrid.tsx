ec4d995ee7f7799306a5b656cef67eba25e9b516
NeroXerith <biene.bryle.sanico@adamson.edu.ph>
2023-05-17 20:36:39 +0800

    Icon and page tite

--- frontend/src/components/datagrid.tsx
+++ frontend/src/components/datagrid.tsx
@@ -150,7 +150,7 @@ const ButtonContainer = styled('div')`
  <Container>
  <BodyContainer>
 <SearchBar searchTerm={searchTerm} handleSearch={handleSearch} />
-<div style={{ height: '100%', width: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+<div style={{ height: '100%', width: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
       <div style={{ height: 'calc(100% - 40px)', width: '100%', margin: 'auto', marginBottom: '20px' }}>
         <DataGrid
           rows={filteredCitizens}
