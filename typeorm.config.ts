4cde4f3c140abcadab472d200aa61dbef8e5e770
Ben-Sicat <bensicat00@gmail.com>
2023-05-22 02:30:10 +0800

    Initial commit

---
+++ typeorm.config.ts
@@ -0,0 +1,13 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { join } from 'path';
+
+export const typeOrmConfig: TypeOrmModuleOptions = {
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'burat',
+    password: 'burat',
+    database: 'burat',
+    entities: [join(__dirname, '../**/*.entity.{js,ts}')],
+    synchronize: true,
+};
