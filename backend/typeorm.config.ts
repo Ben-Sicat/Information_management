73375f0fac22a2c2edfc96affc371465745fc329
Ben-Sicat <bensicat00@gmail.com>
2023-05-22 23:15:00 +0800

    structure

--- backend/typeorm.config.ts
+++ backend/typeorm.config.ts
@@ -1,12 +1,13 @@
 import { TypeOrmModule } from "@nestjs/typeorm";
 
 export const config: TypeOrmModule = {
+    name: 'LocalServer',
     type: 'mysql',
-    host: 'localhost',
+    host: '127.0.0.1',
     port: 3306,
-    username: 'burat',
-    password: 'burat',
-    database: 'burat',
+    username: 'root',
+    password: '',
+    database: 'brgy',
     entities: [__dirname + '/../**/*.entity.{js,ts}'],
     synchronize: true,
 }
