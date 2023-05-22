7f2f53ec633fb18e10243f1b309eded5bac6410f
Ben-Sicat <bensicat00@gmail.com>
2023-05-22 20:30:44 +0800

    re init backend

--- backend/typeorm.config.ts
+++ backend/typeorm.config.ts
@@ -1,14 +1,12 @@
-import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-import { join } from 'path';
+import { TypeOrmModule } from "@nestjs/typeorm";
 
-export const typeOrmConfig: TypeOrmModuleOptions = {
+export const config: TypeOrmModule = {
     type: 'mysql',
     host: 'localhost',
     port: 3306,
     username: 'burat',
     password: 'burat',
     database: 'burat',
-    entities: [join(__dirname, '../**/*.entity.{js,ts}')],
+    entities: [__dirname + '/../**/*.entity.{js,ts}'],
     synchronize: true,
-};
-
+}
